00071535fb65ac81cf496ffd986203b3d1546b12
Sunita <sunita.dhougoda01@gmail.com>
2024-11-26 10:25:06 -0500

    add file

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'sunita.portfolio',
+  appName: 'sunita-portfolio',
+  webDir: 'www'
+};
+
+export default config;
